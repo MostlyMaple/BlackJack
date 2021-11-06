c0a0b18adefb29b1a2989ddb15a08b3083828b4e
Stephen Dean <st793063@dal.ca>
2021-11-06 16:01:25 -0500

    Changed Package Format to allow more scences

--- BlackJackFX/src/application/Main.java
+++ BlackJackFX/src/application/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("Table.fxml"));
+			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("/application/View/BlackJack.fxml"));
 			Scene scene = new Scene(root,800,800);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
