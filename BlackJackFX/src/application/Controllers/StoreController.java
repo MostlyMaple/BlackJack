9bd03de9c395ae14f53f695886ecaf12d99dfbbb
stephanieyounes <stephanie.younes@yahoo.com>
2021-11-28 18:12:00 -0600

    Final Store Commit

--- BlackJackFX/src/application/Controllers/StoreController.java
+++ BlackJackFX/src/application/Controllers/StoreController.java
@@ -19,174 +19,155 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
-
-
 public class StoreController implements Initializable {
 
-    @FXML
-    private ImageView monkey;
+	@FXML
+	private ImageView monkey;
 
-    @FXML
-    private Button noBuyButton;
+	@FXML
+	private Button noBuyButton;
 
-    @FXML
-    private Button mainMenuButton;
+	@FXML
+	private Button mainMenuButton;
 
-    @FXML
-    private TextArea totalBalanceText;
+	@FXML
+	private TextArea totalBalanceText;
 
-    @FXML
-    private Button confirmBuyButton;
+	@FXML
+	private Button confirmBuyButton;
 
-    @FXML
-    private TextArea confirmBuyText;
+	@FXML
+	private TextArea confirmBuyText;
 
-    @FXML
-    private ImageView tree;
+	@FXML
+	private ImageView tree;
 
-    @FXML
-    private ImageView dog;
+	@FXML
+	private ImageView dog;
 
-    @FXML
-    private ImageView food;
-    
-    @Override
+	@FXML
+	private ImageView food;
+
+	// Initialize screen with users current balance
+	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Auto-generated method stub
-    	//confirmBuyText.setText("Click on image to buy!");
-    	totalBalanceText.setText("Current balance: $" + Main.balance);
-		
+		// set totalBalanceText to users current balance
+		totalBalanceText.setText("Current balance: $" + Main.balance);
+
 	}
 
-    @FXML
-    void imageClicked(MouseEvent event)  {
-    	if(event.getPickResult().getIntersectedNode().getId().compareTo("tree")==0) {
-    		
-    		confirmBuyText.setText("Are you sure you want to buy the tree?");
-    
-    		//if confirm button clicked
-    		confirmBuyButton.setOnAction(new EventHandler<ActionEvent>(){
+	// Controls when one of the images are clicked
+	@FXML
+	void imageClicked(MouseEvent event) {
+		// Image clicked Christmas tree
+		if (event.getPickResult().getIntersectedNode().getId().compareTo("tree") == 0) {
 
+			// Confirm users choice
+			confirmBuyText.setText("Are you sure you want to buy the tree?");
+
+			// if confirm button clicked
+			confirmBuyButton.setOnAction(new EventHandler<ActionEvent>() {
+
+				// Handle if user doesn't have enoughtin balance
 				@Override
 				public void handle(ActionEvent event) {
-					// TODO Auto-generated method stub
-					if(Main.balance - 200 < 0) {
-						confirmBuyText.setText("You're not that rich :(");
-					}
-					else {
+					// Print error message if not enough balance
+					if (Main.balance - 200 < 0) {
+						confirmBuyText.setText("You do not have enough money in your balance to buy the tree");
+					} else {
+						// decrease balance and print message if enough
 						Main.balance = Main.balance - 200;
 						totalBalanceText.setText("New Balance:" + Main.balance);
 						confirmBuyText.setText("Merry Christmas!");
 					}
 				}
-    			
-    		});
-    		//if no button clicked
-    		
-    		
-    	}
-    	else if(event.getPickResult().getIntersectedNode().getId().compareTo("dog")==0) {
-    		confirmBuyText.setText("Are you sure you want to buy the dog?");
-    		
-    		//if confirm button clicked
-    		confirmBuyButton.setOnAction(new EventHandler<ActionEvent>(){
 
+			});
+			// if no button clicked
+			// Image clicked dog
+		} else if (event.getPickResult().getIntersectedNode().getId().compareTo("dog") == 0) {
+			// Confirm users choice
+			confirmBuyText.setText("Are you sure you want to buy the dog?");
+
+			// if confirm button clicked
+			confirmBuyButton.setOnAction(new EventHandler<ActionEvent>() {
+
+				// Handle if user doesn't have enoughtin balance
 				@Override
 				public void handle(ActionEvent event) {
-					// TODO Auto-generated method stub
-					if(Main.balance - 400 < 0) {
+					// Print error message if not enough balance
+					if (Main.balance - 400 < 0) {
 						confirmBuyText.setText("You do not have enough money in your balance to buy the dog");
-					}
-					else {
+					} else {
+						// decrease balance and print message if enough
 						Main.balance = Main.balance - 400;
 						totalBalanceText.setText("New Balance:" + Main.balance);
 						confirmBuyText.setText("Congrats on the new dog!");
 					}
 				}
-    			
-    		});
-    			
-    		
-    		
-    		//if no button clicked
-    		//confirmBuyText.setText("Click on image to buy!");
-    		
-    	}
-    	else if(event.getPickResult().getIntersectedNode().getId().compareTo("monkey")==0) {
-    		confirmBuyText.setText("Are you sure you want to buy the monkey?");
-    		
-    		//if confirm button clicked
-    		confirmBuyButton.setOnAction(new EventHandler<ActionEvent>(){
+
+			});
+
+			// Image clicked monkey
+		} else if (event.getPickResult().getIntersectedNode().getId().compareTo("monkey") == 0) {
+			// Confirm users choice
+			confirmBuyText.setText("Are you sure you want to buy the monkey?");
+
+			// if confirm button clicked
+			confirmBuyButton.setOnAction(new EventHandler<ActionEvent>() {
 
 				@Override
 				public void handle(ActionEvent event) {
-					// TODO Auto-generated method stub
-					if(Main.balance - 500 < 0) {
+					// Print error message if not enough balance
+					if (Main.balance - 500 < 0) {
 						confirmBuyText.setText("You do not have enough money in your balance to buy the monkey");
-					}
-					else {
+					} else {
+						// decrease balance and print message if enough
 						Main.balance = Main.balance - 500;
 						totalBalanceText.setText("New Balance:" + Main.balance);
 						confirmBuyText.setText("Congrats on the new monkey!");
 					}
 				}
-    			
-    		});
-    		//if no button clicked
-    		
-    		
-    		
-    	}
-    	else if(event.getPickResult().getIntersectedNode().getId().compareTo("food")==0) {
-    		confirmBuyText.setText("Are you sure you want to buy the meal?");
-    		
-    		//if confirm button clicked
-    		confirmBuyButton.setOnAction(new EventHandler<ActionEvent>(){
+
+			});
+
+			// Image clicked food
+		} else if (event.getPickResult().getIntersectedNode().getId().compareTo("food") == 0) {
+			confirmBuyText.setText("Are you sure you want to buy the meal?");
+
+			// if confirm button clicked
+			confirmBuyButton.setOnAction(new EventHandler<ActionEvent>() {
 
 				@Override
 				public void handle(ActionEvent event) {
-					// TODO Auto-generated method stub
-					if(Main.balance - 100 < 0) {
+					// Print error message if not enough balance
+					if (Main.balance - 100 < 0) {
 						confirmBuyText.setText("You do not have enough money in your balance to buy the meal");
-					}
-					else {
+					} else {
+						// decrease balance and print message if enough
 						Main.balance = Main.balance - 100;
 						totalBalanceText.setText("New Balance:" + Main.balance);
 						confirmBuyText.setText("Enjoy your food!");
 					}
-		    		
+
 				}
-    			
-    		});
-    		
-    		
-    	}
-    	
-    }
-    @FXML
-    void mainMenuHandle(ActionEvent event) throws IOException {
-    	URL url = new File("src/application/View/MainMenu.fxml").toURI().toURL();
-    	AnchorPane mainPane = FXMLLoader.load(url);
-    	Scene scene = new Scene(mainPane);
-    	Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
-    	window.setScene(scene);
-    	window.show();
 
-    }
+			});
 
+		}
 
+	}
 
-    @FXML
-    void changeMindHandle(ActionEvent event) {
+	// Takes user back to Main Meny once main menuu button is clicked
+	@FXML
+	void mainMenuHandle(ActionEvent event) throws IOException {
+		URL url = new File("src/application/View/MainMenu.fxml").toURI().toURL();
+		AnchorPane mainPane = FXMLLoader.load(url);
+		Scene scene = new Scene(mainPane);
+		Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		window.setScene(scene);
+		window.show();
 
-    }
-
-
-
-
-	
-
-
+	}
 
 }
-
